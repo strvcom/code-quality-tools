fc9b9d4f54dc68ea6072d3f1c2681fe024152f11
Robert Rossmann <rr.rossmann@me.com>
2019-03-07 12:42:10 +0100

    chore(remark): add header to source files

--- packages/remark-config/index.js
+++ packages/remark-config/index.js
@@ -1,3 +1,11 @@
+/**
+ * strvcom/eslint-config-node
+ *
+ * @author      Robert Rossmann <rr.rossmann@me.com>
+ * @copyright   2019 STRV
+ * @license     http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
+ */
+
 'use strict'
 
 module.exports = {
