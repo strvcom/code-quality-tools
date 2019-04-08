d3fbe9ef9967297589cc4bb5ca91cfe76ca1b175
André Nanninga <andre@nannin.ga>
2019-04-08 15:04:26 +0200

    chore: updated package-lock, readme, removed changelog

--- packages/eslint-config-react-native/index.js
+++ packages/eslint-config-react-native/index.js
@@ -1,61 +1,61 @@
 /**
  *
- * @author      André Nanninga <andre@nannin.ga>
+ * @author      André Nanninga <andre.nanninga@strv.com>
  * @copyright   2019 STRV
  * @license     http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
  */
 
-'use strict'
+"use strict";
 
 module.exports = {
-  extends: [require.resolve('@strv/eslint-config-react')],
+  extends: [require.resolve("@strv/eslint-config-react")],
 
   env: {
-    'react-native/react-native': true,
+    "react-native/react-native": true
   },
 
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module",
     ecmaVersion: 2017,
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
 
-  plugins: ['react', 'react-native', 'react-native-a11y'],
+  plugins: ["react", "react-native", "react-native-a11y"],
 
   rules: {
     // Detect StyleSheet rules which are not used in your React components.
     // When working on a component over a longer period of time, you could end up with unused
     // StyleSheet rules that slipped in over time but are forgotten as you continue to improve your
     // UX/UI design.
-    'react-native/no-unused-styles': 'warn',
+    "react-native/no-unused-styles": "warn",
 
     // Enforce using platform specific filenames when necessary.
     // When working on a project that supports both Android and IOS, you have to make sure that you
     // use platform specific filenames when you use platform specific components to produce the
     // correct bundle per platform.
-    'react-native/split-platform-components': 'error',
+    "react-native/split-platform-components": "error",
 
     // Detect JSX components with inline styles that contain literal values.
     // This rule detects inline style objects when they contain literal values. If inline styles
     // only contain variable values, the style is considered acceptable because it's sometimes
     // necessary to set styles based on state or props.
-    'react-native/no-inline-styles': 'warn',
+    "react-native/no-inline-styles": "warn",
 
     // Detect StyleSheet rules and inline styles containing color literatls instead of variables.
     // The rule looks at all properties that contain color (case-insensitive) in their name in
     // either StyleSheet definitions or JSX properties that have style in their name.
-    'react-native/no-color-literals': 'warn',
+    "react-native/no-color-literals": "warn",
 
     // Detect raw text outside of Text component.
     // All strings in React NAtive should be wrapped with a Text component. Use `skip` to enable
     // text in custom components. Handy for styled-components.
-    'react-native/no-raw-text': [
-      'error',
+    "react-native/no-raw-text": [
+      "error",
       {
-        skip: [],
-      },
+        skip: []
+      }
     ],
 
     // Enforce that views that have accessible={true}, also have an accessibilityLabel prop.
@@ -63,51 +63,51 @@ module.exports = {
     // accessibilityLabel on the element, so that people who use VoiceOver know what element they
     // have selected. VoiceOver will read the property value when a user selects the associated
     // element.
-    'react-native-a11y/accessibility-label': 'error',
+    "react-native-a11y/accessibility-label": "error",
 
     // Enforce all <Touchable\*> components have accessibilityRole prop or both accessibilityTraits
     // and accessibilityComponentType props set.
     // <Touchable*> components must have the accessibilityRole prop or both accessibilityTraits and
     // accessibilityComponentType props in order to be fully accessible.
-    'react-native-a11y/has-accessibility-props': 'error',
+    "react-native-a11y/has-accessibility-props": "error",
 
     // Enforce accessibilityComponentType property value is valid.
     // The accessibilityComponentType property is essentially the android version of
     // accessibilityTraits and is used to alert a user, using TalkBack, what kind of element they
     // have selected.
-    'react-native-a11y/has-valid-accessibility-component-type': 'error',
+    "react-native-a11y/has-valid-accessibility-component-type": "error",
 
     // Enforce accessibilityLiveRegion prop values must be valid.
     // On android devices, when components dynamically change, we want TalkBack to alert the end
     // user. This is made possible by the accessibilityLiveRegion property.
-    'react-native-a11y/has-valid-accessibility-live-region': 'error',
+    "react-native-a11y/has-valid-accessibility-live-region": "error",
 
     // Enforce accessibilityRole property value is valid.
     // The accessibilityRole property is used to tell Talkback or Voiceover the role of a UI
     // Element.
-    'react-native-a11y/has-valid-accessibility-role': 'error',
+    "react-native-a11y/has-valid-accessibility-role": "error",
 
     // Enforce accessibilityStates property value is valid.
     // The accessibilityStates property is used to tell Talkback or Voiceover the state a UI Element
     // is in.
-    'react-native-a11y/has-valid-accessibility-states': 'error',
+    "react-native-a11y/has-valid-accessibility-states": "error",
 
     // Enforce accessibilityTraits and accessibilityComponentType prop values must be valid.
     // The accessibilityTraits property is used to tell a person using VoiceOver what kind of
     // element they have selected.
-    'react-native-a11y/has-valid-accessibility-traits': 'error',
+    "react-native-a11y/has-valid-accessibility-traits": "error",
 
     // Enforce importantForAccessibility property value is valid.
     // If there are two (or more) overlapping UI components with the same parent, default
     // accessibility focus can have unpredictable behavior. The importantForAccessibility property
     // will resolve this by controlling if a view fires accessibility events and if it is reported
     // to accessibility services.
-    'react-native-a11y/has-valid-important-for-accessibility': 'error',
+    "react-native-a11y/has-valid-important-for-accessibility": "error",
 
     // Enforce if a view has accessible={true}, that there are no touchable elements inside.
     // <Touchable*> or will not work inside an accessible element. Any element that has the
     // accessible={true} property (along with the accessibleLabel property) must therefore not
     // contain any <Touchable*> or elements.
-    'react-native-a11y/no-nested-touchables': 'error',
-  },
-}
+    "react-native-a11y/no-nested-touchables": "error"
+  }
+};
--- packages/eslint-config-react-native/readme.md
+++ packages/eslint-config-react-native/readme.md
@@ -2,9 +2,27 @@
 
 > STRV's ESLint config for React-Native projects
 
-Suitable for linting React Native source files. This ruleset extends `@strv/eslint-config-react` but does not extend `@strv/eslint-config-react/optional` nor `@strv/eslint-config-react/style`.
+Suitable for linting React Native source files. This ruleset extends `@strv/eslint-config-react`.
 
-This rulest includes accessibility rules, based on [eslint-plugin-react-native-a11y][a11y-repo], by default.
+This rulest includes accessibility rules, based on [eslint-plugin-react-native-a11y][a11y-repo].
+
+## Configurations
+
+### `@strv/eslint-config-react-native`
+
+Suitable for linting React Native projects.
+
+### `@strv/eslint-config-react-native/optional`
+
+Use this ruleset in conjunction with the above base ruleset. Provides additional insights into potential inconsistencies in the project. Extends `@strv/eslint-config-react/optional`.
+
+> For new projects, it is recommended to enable this ruleset. For existing projects, it is only recommended for the brave.
+
+## Coding styles
+
+### `@strv/eslint-config-react-native/style`
+
+This ruleset includes rules which deal with how the code looks like and not how it works. It helps keeping the code clean and consistent. Extends `@strv/eslint-config-react/style`. 🎨
 
 A full configuration for a React Native project:
 
@@ -15,8 +33,8 @@ A full configuration for a React Native project:
 module.exports = {
   extends: [
     "@strv/eslint-config-react-native",
-    "@strv/eslint-config-react/optional",
-    "@strv/eslint-config-react/style"
+    "@strv/eslint-config-react-native/optional",
+    "@strv/eslint-config-react-native/style"
   ]
 };
 ```
