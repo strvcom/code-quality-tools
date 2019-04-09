26478d0d3ab6c736d37eb08210ea4b67415cf029
Robert Rossmann <rr.rossmann@me.com>
2019-04-09 11:26:21 +0200

    feat(eslint-react): add optional ruleset

---
+++ packages/eslint-config-react/optional.js
@@ -0,0 +1,14 @@
+/**
+ * strvcom/eslint-config-react
+ *
+ * @author      Robert Rossmann <rr.rossmann@me.com>
+ * @copyright   2019 STRV
+ * @license     http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
+ */
+
+'use strict'
+
+module.exports = {
+
+  extends: require.resolve('@strv/eslint-config-base/optional'),
+}
