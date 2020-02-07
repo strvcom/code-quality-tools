4b8d29bf82dcbbea0ad33b7cd0beae3706e8af54
Robert Rossmann <rr.rossmann@me.com>
2020-02-07 14:45:42 +0100

    build: commit new release changes locally

--- utils/make/release.sh
+++ utils/make/release.sh
@@ -2,13 +2,23 @@
 
 set -o errexit
 
+head=$(git rev-parse --abbrev-ref HEAD)
+
 printf "\n=====>\tCreating release branch...\n"
 
-git branch --force release
+git checkout -b release
+
+printf "\n=====>\tCreating release commit...\n"
+
+npx lerna version
 
 printf "\n=====>\tPushing...\n"
 
 git push --set-upstream origin release:release
+
+printf "\n=====>\tSwitching back to previous branch...\n"
+
+git checkout "${head}"
 
 printf "\n=====>\tDeleting local release branch...\n"
 
