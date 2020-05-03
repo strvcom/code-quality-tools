6980bb4a5ab8d6643004c8d84a107bea67a1f101
Michal Jarnot <m.jarnot@yahoo.com>
2020-05-03 18:01:10 +0200

    feat(eslint-graphql): add configuration graphql and apollo

---
+++ packages/eslint-config-graphql/index.js
@@ -0,0 +1,18 @@
+/**
+ * strvcom/eslint-config-graphql
+ *
+ * @author      Michal Jarnot <m.jarnot@yahoo.com>
+ * @copyright   2020 STRV
+ * @license     http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
+ */
+
+'use strict'
+
+module.exports = {
+  plugins: ['graphql'],
+  rules: {
+    'graphql/template-strings': ['error', {
+      'env': 'apollo',
+    }],
+  },
+}
