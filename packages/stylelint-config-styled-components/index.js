357e59c1b31f437513d227710faa57c11ebcff2f
Michal Jarnot <m.jarnot@yahoo.com>
2020-05-06 01:08:14 +0200

    feat: update styled-components stylelint config
    - remove processor styled components
    - extend config base

--- packages/stylelint-config-styled-components/index.js
+++ packages/stylelint-config-styled-components/index.js
@@ -1,6 +1,34 @@
 'use strict'
 
 module.exports = {
-  processors: ['stylelint-processor-styled-components'],
-  extends: ['stylelint-config-standard', 'stylelint-config-styled-components'],
+  extends: [
+    '@code-quality/stylelint-base-config',
+  ],
+  rules: {
+    //
+    // Turn off base rules
+    //
+
+    // Possible errors
+
+    'font-family-no-missing-generic-family-keyword': null,
+    'no-empty-source': null,
+    'string-no-newline': null,
+    'selector-pseudo-class-no-unknown': null,
+    'selector-pseudo-element-no-unknown': null,
+    'selector-type-no-unknown': null,
+    'no-descending-specificity': null,
+
+    // Limit language features
+
+    'selector-max-empty-lines': null,
+    'selector-no-vendor-prefix': null,
+
+    // Stylistic issues
+
+    'no-missing-end-of-source-newline': null,
+    'function-name-case': null,
+    'value-keyword-case': null,
+    'no-empty-first-line': null,
+  }
 }
