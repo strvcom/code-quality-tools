d550db815c045da5603929aa1fdb199dd08706bf
Robert Rossmann <rr.rossmann@me.com>
2021-09-07 00:29:48 +0200

    chore(repo): update Commitlint scopes (experimental)

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -7,17 +7,13 @@ module.exports = {
 
   rules: {
     'scope-enum': [1, 'always', [
-      'commitlint',
-      'eslint-base',
-      'eslint-mocha',
-      'eslint-node',
-      'eslint-react',
-      'eslint-typescript',
-      'eslint-react-native',
-      'prettier',
-      'remark',
-      'stylelint',
-      'textlint',
+      // Use when committing rule changes/additions/removals etc.
+      'rule',
+      // Use when updating dependencies in rulesets
+      'deps',
+      // Use for anything that does not directly affect rulesets, ie. updating repo-wide
+      // dependencies etc.
+      'repo',
     ]],
 
     'footer-max-line-length': [0],
