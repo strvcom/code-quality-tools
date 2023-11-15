122a63e3624072c241d2880882378761d61f9b82
Petr Konecny <petr.konecny@strv.com>
2023-11-15 16:09:40 +0100

    chore: updated license

--- packages/eslint-config-react-native/index.js
+++ packages/eslint-config-react-native/index.js
@@ -2,7 +2,7 @@
  * strvcom/eslint-config-react-native
  *
  * @author      Petr Konecny <petr.konecny@strv.com>
- * @copyright   2016-present Expo, 2023-present STRV
+ * @copyright   2023-present STRV
  * @license     http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
  */
 
