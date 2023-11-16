bef4ced1f9e79ae70d070f1ea326300e44ea0de4
Petr Konecny <petr.konecny@strv.com>
2023-11-16 13:14:41 +0100

    chore: added ts analysis

--- packages/eslint-config-react-native/index.js
+++ packages/eslint-config-react-native/index.js
@@ -9,12 +9,38 @@
 'use strict'
 
 module.exports = {
-  extends: ['universe/native'],
+  extends: [
+    'universe/native',
+    'universe/shared/typescript-analysis',
+    require.resolve('@strv/eslint-config-react/style'),
+  ],
   rules: {
-    // enforces hooks have dependencies listed in the dependency arrays
+    // overrides the Expo Universe config, enforces hooks have dependencies listed in the dependency arrays
     // preventing issues with hooks not running correctly when dependency values change
     // Original expo config disabled this because they felt it is returning too many false positives
     // https://github.com/expo/expo/pull/9130
     'react-hooks/exhaustive-deps': 'warn',
+
+    // overrides the base config, creates issues with import rules setup in the Expo universe config
+    'padding-line-between-statements': 'off',
+
+    // overrides base config, adds exceptions to the id-length rule from base config
+    // coordinates are often used in React Native and it is not a good idea to rename them
+    // also there is a common translation function 't' which is also not a good candidate for renaming
+    'id-length': ['warn', { properties: 'never', exceptions: ['x', 'y', 'z', 't', '_'] }],
+
+    // overrides the Expo Universe config, as we feel there is a value in having to specifically mark floating
+    // promises as void
+    'no-void': 'off',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      rules: {
+        // Adds a new rule from as we feel there is a value in having to specifically mark floating
+        // promises as void
+        '@typescript-eslint/no-floating-promises': 'error',
+      },
+    },
+  ],
 }
