7eb52e597d47c43d7f4b7fb1cb309713ec6aa015
Robert Rossmann <rr.rossmann@me.com>
2025-05-30 00:49:16 +0200

    chore: use ESLint's `defineConfig()`

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,19 @@
+import { defineConfig } from 'eslint/config'
 import node from './packages/eslint-config-node/index.mjs'
 import optional from './packages/eslint-config-node/optional.mjs'
 import style from './packages/eslint-config-node/style.mjs'
 
-/** @type {Array<import("eslint").Linter.Config>} */
-const config = [{
-  linterOptions: {
-    reportUnusedDisableDirectives: true,
-  },
-  ignores: [
-    'node_modules',
-    '!.*.js',
+export default defineConfig([{
+  files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
+  extends: [
+    node,
+    optional,
+    style,
   ],
+  linterOptions: {
+    reportUnusedDisableDirectives: 'warn',
+  },
   settings: {
     'import/resolver': { typescript: {} },
   },
-},
-node,
-optional,
-style,
-{
-  rules: {},
-}]
-
-export default config
+}])
